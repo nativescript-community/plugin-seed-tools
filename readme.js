4e3caeef2401f28e7d1d3d0e9511536392ba327e
Martin Guillon <martin.guillon@akylas.fr>
2022-05-13 11:21:24 +0200

    fix: readme creation on macos

--- readme.js
+++ readme.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
+const path = require('path');
 const exec = require('child_process').exec;
 
 const pluginConfig = fs.readFileSync('config.json');
 const pluginConfigJson = JSON.parse(pluginConfig);
+
+function listFoldersSync(dir) {
+    const folders = [];
+    fs.readdirSync(dir).forEach((f) => {
+        const dirPath = path.join(dir, f);
+        const isDirectory = fs.statSync(dirPath).isDirectory();
+        if (isDirectory) {
+            folders.push(f);
+        }
+    });
+    return folders;
+}
 
 let enabled = false;
 
@@ -11,43 +24,42 @@ if ('readme' in pluginConfigJson) {
 }
 
 if (enabled) {
-    fs.readdir('./packages', (error, files) => {
-        const packagesCount = files.length;
+    const files = listFoldersSync('./packages');
+    const packagesCount = files.length;
 
-        let readmeOutput = '';
+    let readmeOutput = '';
 
-        if (packagesCount > 1) {
-            readmeOutput += 'This monorepo contains multiple packages:<br><br>';
-        }
+    if (packagesCount > 1) {
+        readmeOutput += 'This monorepo contains multiple packages:<br><br>';
+    }
 
-        for (const package of files) {
-            const multiPackageMarkdown = `<details>
+    for (const package of files) {
+        const multiPackageMarkdown = `<details>
 <summary><b>${package}</b></summary>
 
 {{ load:packages/${package}/README.md }}
 </details>`;
 
-            const singlePackageMarkdown = `{{ load:packages/${package}/README.md }}`;
+        const singlePackageMarkdown = `{{ load:packages/${package}/README.md }}`;
 
-            if (packagesCount > 1) {
-                readmeOutput += multiPackageMarkdown;
-            } else {
-                readmeOutput += singlePackageMarkdown;
-            }
+        if (packagesCount > 1) {
+            readmeOutput += multiPackageMarkdown;
+        } else {
+            readmeOutput += singlePackageMarkdown;
         }
+    }
 
-        try {
-            fs.writeFileSync('blueprint.md', readmeOutput);
-        } catch (err) {
-            console.error(err);
+    try {
+        fs.writeFileSync('blueprint.md', readmeOutput);
+    } catch (err) {
+        console.error(err);
+    }
+
+    exec('./node_modules/.bin/readme generate -c ./tools/readme/blueprint.json', function (err, stdout, stderr) {
+        if (err) {
+            console.log(stderr);
         }
-
-        exec('./node_modules/.bin/readme generate -c ./tools/readme/blueprint.json', function (err, stdout, stderr) {
-            if (err) {
-                console.log(stderr);
-            }
-            console.log(stdout);
-        });
+        console.log(stdout);
     });
 } else {
     console.log('README generation is disabled. To enable, add "readme": true to the config.json');
